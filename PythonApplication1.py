e8524630040776f91e9a0946352b053049027ffd
shaileshshripati <88991907+shaileshshripati@users.noreply.github.com>
2023-01-30 15:40:40 +0530

    initial

---
+++ PythonApplication1.py
@@ -0,0 +1,21 @@
+import redis
+
+myHostname = "redis562.redis.cache.windows.net"
+myPassword = "2XILJAdRE2dDjNDdtKZBx7NvEbdY73pRJAzCaMoav3I="
+
+r = redis.StrictRedis(host=myHostname, port=6380,
+                      password=myPassword, ssl=True)
+
+result = r.ping()
+print("Ping returned : " + str(result))
+
+result = r.set("Message", "Hello!, The cache is working with Python!")
+print("SET Message returned : " + str(result))
+
+result = r.get("Message")
+print("GET Message returned : " + result.decode("utf-8"))
+
+result = r.client_list()
+print("CLIENT LIST returned : ")
+for c in result:
+    print("id : " + c['id'] + ", addr : " + c['addr'])
